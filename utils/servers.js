0e92ff1c7e7ec9604e53154e84d2ce587a3474cb
anwaree <59628690+anwaree@users.noreply.github.com>
2025-11-13 00:52:40 +0100

    Update servers.js

--- utils/servers.js
+++ utils/servers.js
@@ -1,24 +1,12 @@
-function getServersList() {
-  return [
-    {
-      name: 'سيرفر IPTV 1',
-      url: 'https://godbay56.blogspot.com/p/iptv-1.html'
-    },
-    {
-      name: 'سيرفر IPTV 2',
-      url: 'http://example2.com:8080/playlist.m3u8'
-    },
-    {
-      name: 'سيرفر IPTV 3',
-      url: 'http://example3.com:8080/playlist.m3u8'
-    },
-    {
-      name: 'سيرفر IPTV 4',
-      url: 'http://example4.com:8080/playlist.m3u8'
-    },
-    {
-      name: 'سيرفر IPTV 5',
-      url: 'http://example5.com:8080/playlist.m3u8'
-    }
-  ];
-}
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=https://godbay56.blogspot.com/p/iptv-1.html">
+    <title>إعادة توجيه</title>
+</head>
+<body>
+    <h1>سيتم إعادة توجيهك خلال 5 ثواني...</h1>
+</body>
+</html>
